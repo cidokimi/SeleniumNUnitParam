3f4df9a80d58a8ea0f160ce67fd576c087caa3c7
cidokimi <31003683+cidokimi@users.noreply.github.com>
2019-06-08 14:13:06 +0300

    Update BrowserTest.cs

--- SeleniumNUnitParam/BrowserTest.cs
+++ SeleniumNUnitParam/BrowserTest.cs
@@ -11,6 +11,17 @@ namespace SeleniumNUnitParam
     public class BrowserTest : Hooks
     {
 
+        [Test]
+        public void GoogleTest()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("Selenium");
+            System.Threading.Thread.Sleep(5000);
+            Driver.FindElement(By.Name("btnK")).Click();
+            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                                            "The text selenium doest not exist");
+
+        }
 
         [Test]
         public void ExecuteAutomationTest()
@@ -24,6 +35,18 @@ namespace SeleniumNUnitParam
                                             "The text selenium doest not exist");
 
         }
+        /*[Test]
+        public void ExecuteAutomationTest()
+        {
+            Driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
+            Driver.FindElement(By.Name("UserName")).SendKeys("admin");
+            Driver.FindElement(By.Name("Password")).SendKeys("admin");
+            Driver.FindElement(By.Name("Login")).Submit();
+            System.Threading.Thread.Sleep(2000);
+            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                                            "The text selenium doest not exist");
+
+        }*/
        /* [Test]
         public void GoogleTest()
         {
